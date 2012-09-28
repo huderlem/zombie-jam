593eb76b384f2bf753adbeb2edc5ab8ab67b0ea1
Marcus Huderle <huderlem@gmail.com>
2012-09-27 22:03:32 -0500

    Walls are now illuminated and remembered for a fixed duration.

--- src/GridSpace.java
+++ src/GridSpace.java
@@ -15,6 +15,8 @@ public class GridSpace extends Entity {
 	Image texture;
 	public int type;	// 0=floor; 1=wall
 	private boolean shouldBeDrawn = true;
+	private int wallLitCounter = -1;
+	private static final int WALL_LIT_DURATION = 6000;
 	
 	public GridSpace(Entity parent, int spaceType, float x, float y) {
 		super(parent);
@@ -33,6 +35,12 @@ public class GridSpace extends Entity {
 	@Override
 	public void update(GameContainer gc, int delta, GameplayState game) {
 		super.update(gc, delta, game);
+		if (wallLitCounter >= 0) {
+			wallLitCounter -= delta;
+			if (wallLitCounter < 0) {
+				EntityManager.litWalls.remove(id());
+			}
+		}
 	}
 
 	@Override
@@ -68,6 +76,16 @@ public class GridSpace extends Entity {
 	
 	public void setShouldBeDrawn(boolean a) {
 		this.shouldBeDrawn = a;
+	}
+	
+	public void illuminateWall() {
+		EntityManager.litWalls.put(id(), this);
+		wallLitCounter = WALL_LIT_DURATION;
+	}
+	
+	public Color getIlluminationAlpha() {
+		float alpha = wallLitCounter/(float)WALL_LIT_DURATION;
+		return new Color(1, 1, 1, alpha);
 	}
 
 }
